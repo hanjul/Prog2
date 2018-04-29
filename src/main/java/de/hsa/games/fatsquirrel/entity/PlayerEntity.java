486d9543bc63a2e9ce05101a4861bce5a6f86c81
hanjul <>
2018-04-29 19:02:06 +0200

    Add stun mechanism

--- src/main/java/de/hsa/games/fatsquirrel/entity/PlayerEntity.java
+++ src/main/java/de/hsa/games/fatsquirrel/entity/PlayerEntity.java
@@ -1,10 +1,26 @@
 package de.hsa.games.fatsquirrel.entity;
 
 import de.hsa.games.fatsquirrel.core.XY;
+import de.hsa.games.fatsquirrel.util.Assert;
 
 public abstract class PlayerEntity extends Character {
 
+	private int stunnedRounds;
+	
 	public PlayerEntity(int id, int energy, XY location) {
 		super(id, energy, location);
+	}
+	
+	protected boolean isStunnedAndDecrement() {
+		return stunnedRounds-- > 0;
+	}
+	
+	public boolean isStunned() {
+		return stunnedRounds > 0;
+	}
+	
+	public void setStunnedRounds(final int stunnedRounds) {
+		Assert.isTrue(stunnedRounds >= 0, "stunnedRounds must be >= 0");
+		this.stunnedRounds = stunnedRounds;
 	}
 }
