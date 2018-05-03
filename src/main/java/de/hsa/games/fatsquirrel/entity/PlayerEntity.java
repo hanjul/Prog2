4effdb1e773b1232578c5b74461ea57be04d3526
hanjul <>
2018-05-03 18:22:15 +0200

    Move stun decrement logic to PlayerEntity

--- src/main/java/de/hsa/games/fatsquirrel/entity/PlayerEntity.java
+++ src/main/java/de/hsa/games/fatsquirrel/entity/PlayerEntity.java
@@ -1,5 +1,6 @@
 package de.hsa.games.fatsquirrel.entity;
 
+import de.hsa.games.fatsquirrel.core.EntityContext;
 import de.hsa.games.fatsquirrel.core.XY;
 import de.hsa.games.fatsquirrel.util.Assert;
 
@@ -11,8 +12,16 @@ public abstract class PlayerEntity extends Character {
 		super(id, energy, location);
 	}
 	
+	@Override
+	public void nextStep(EntityContext context) {
+		Assert.notNull(context, "context must not be null");
+		if (stunnedRounds >= 0) {
+			stunnedRounds--;
+		}
+	}
+	
 	public boolean isStunned() {
-		return stunnedRounds > 0;
+		return stunnedRounds >= 0;
 	}
 	
 	public void stun(final int stunnedRounds) {
